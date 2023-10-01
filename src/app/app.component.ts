096a49d6caf373f963153fef766e1f61b6436e82
zackproject <zackprojectapp@gmail.com>
2023-10-01 19:25:44 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+// Aixo son les props del projecte, es poden cridar en el 'html'
 export class AppComponent {
   title = 'zksama-app';
+
+  changeTitle() {
+    this.title = "canviat";
+  }
 }
